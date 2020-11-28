bca7d075850bae63273b2ba90ba51ce3235d2089
a0079 <74526029+a0079@users.noreply.github.com>
2020-11-28 16:22:28 +0800

    Update AdafruitDHT.py

--- examples/AdafruitDHT.py
+++ examples/AdafruitDHT.py
@@ -1,7 +1,6 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # Copyright (c) 2014 Adafruit Industries
 # Author: Tony DiCola
-
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
@@ -19,9 +18,40 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-import sys
 
+import sys
+import socket
+import threading
+import RPi.GPIO as GPIO
+import http.client as http
+import urllib
+import json
+import time
 import Adafruit_DHT
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+deviceId = 'DNVEDCFZ'
+deviceKey = '489vJVLK2yn4fXfD' 
+
+def post_to_mcs(payload): 
+	headers = {"Content-type": "application/json", "deviceKey": deviceKey} 
+	not_connected = 1 
+	while (not_connected):
+		try:
+			conn = http.HTTPConnection("api.mediatek.com:80")
+			conn.connect() 
+			not_connected = 0 
+		except (http.HTTPException, socket.error) as ex: 
+			print ("Error: %s" % ex)
+			time.sleep(10)
+			 # sleep 10 seconds 
+	conn.request("POST", "/mcs/v2/devices/" + deviceId + "/datapoints", json.dumps(payload), headers) 
+	response = conn.getresponse() 
+	print( response.status, response.reason, json.dumps(payload), time.strftime("%c")) 
+	data = response.read() 
+	conn.close() 
+ 
 
 
 # Parse command line parameters.
@@ -52,53 +82,22 @@ if humidity is not None and temperature is not None:
 else:
     print('Failed to get reading. Try again!')
     sys.exit(1)
-while true:
-	h0, t0 = Adafruit_DHT.read_retry(sensor, pin)
+while(1):
+	h0,t0 = Adafruit_DHT.read_retry(sensor,pin)
+	SwitchStatus = GPIO.input(24)
 	if h0 is not None and t0 is not None:
-		print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(t0, h0))
+		print('Temp={0:0.1f}* Humidity={1:0.1f}%'.format(t0,h0))
+		payload = {"datapoints":[{"dataChnId":"Humidity","values":{"value":h0}},{"dataChnId":"Temperature","values":{"value":t0}},{"dataChnId":"SwitchStatus","values":{"value":SwitchStatus}}]} 
+		post_to_mcs(payload)
+		time.sleep(10)
+		if( SwitchStatus == 0):
+			print('Button pressed')
+		else:
+			print('Button released')
 	else:
 		print('Failed to get reading. Try again!')
 		sys.exit(1)
 
-#/usr/bin/python3
-import time
-import sys
-import http.client as http
-import urllib
-import json
-deviceId = "D4IRQlOR“
-deviceKey = “zDqZcMS7d0kXjO0B“ 
-def post_to_mcs(payload): 
-	headers = {"Content-type": "application/json", "deviceKey": deviceKey} 
-	not_connected = 1 
-	while (not_connected):
-		try:
-			conn = http.HTTPConnection("api.mediatek.com:80")
-			conn.connect() 
-			not_connected = 0 
-		except (http.HTTPException, socket.error) as ex: 
-			print ("Error: %s" % ex)
- 			time.sleep(10)
-			 # sleep 10 seconds 
-	conn.request("POST", "/mcs/v2/devices/" + deviceId + "/datapoints", json.dumps(payload), headers) 
-	response = conn.getresponse() 
-	print( response.status, response.reason, json.dumps(payload), time.strftime("%c")) 
-	data = response.read() 
-	conn.close() 
-
-
-while true:
-h0, t0= Adafruit_DHT.read_retry(sensor, pin)
-if h0 is not None and t0 is not None:
-	print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(t0, h0))
-
-	payload = {"datapoints":[{"dataChnId":"Humidity","values":{"value":h0}},
-		{"dataChnId":"Temperature","values":{"value":t0}}]} 
-	post_to_mcs(payload)
-	time.sleep(10) 
-
-else:
-	print('Failed to get reading. Try again!')
-	sys.exit(1)
-
+	
+	
 
